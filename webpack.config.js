1def81650cf6e2a66d516d2a2f1f109350ce3cf9
Preeti Roshan <preetiroshan14@gmail.com>
2024-03-23 20:49:58 +0530

    feat: added dotenv package

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const DotEnv = require("dotenv-webpack");
 
 module.exports = {
   mode: "development",
@@ -16,7 +17,10 @@ module.exports = {
       },
     ],
   },
-  plugins: [new HTMLWebpackPlugin({ template: "./src/index.html" })],
+  plugins: [
+    new HTMLWebpackPlugin({ template: "./src/index.html" }),
+    new DotEnv(),
+  ],
   devtool: "inline-source-map",
   devServer: {
     hot: true,
